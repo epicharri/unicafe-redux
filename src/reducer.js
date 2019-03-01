3426287aefaeb5d35f94d8bef91ef391da13951c
Kähkönen Harri J <harrikah@lx8-523-012.pc.helsinki.fi>
2019-03-01 10:29:49 +0200

    napit toimivat

--- src/reducer.js
+++ src/reducer.js
@@ -8,12 +8,18 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
+      state.good = state.good + 1
       return state
     case 'OK':
+      state.ok = state.ok + 1
       return state
     case 'BAD':
+      state.bad = state.bad + 1
       return state
     case 'ZERO':
+      state.good = 0
+      state.ok = 0
+      state.bad = 0
       return state
     default: return state
   }
